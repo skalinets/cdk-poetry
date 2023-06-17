2609942b8b4ae4da9ba689da8cafdd596552511d
Serhii Kalinets <kalinets@gmail.com>
2023-06-17 14:34:36 +0300

    chore: Initial commit

---
+++ tests/unit/test_cdk_poetry_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from cdk_poetry.cdk_poetry_stack import CdkPoetryStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cdk_poetry/cdk_poetry_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = CdkPoetryStack(app, "cdk-poetry")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
