1092fd042ca5803fdd83f64fe9fb636e7b42c510
Serhii Kalinets <kalinets@gmail.com>
2023-06-19 17:49:09 +0300

    chore: Code reformat

--- cdk_poetry/lambdas/hello_world/hello_world/index.py
+++ cdk_poetry/lambdas/hello_world/hello_world/index.py
@@ -1,5 +1,7 @@
 from common.helper import get_answer
+
+
 def handler(event, context):
     f = get_answer("42")
-    return(f"42: {f}")
+    return f"42: {f}"
     # return {"statusCode": 200, "body": "hello"}
