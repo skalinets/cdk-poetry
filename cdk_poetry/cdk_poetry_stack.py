184b0fe1d31afe7e4e89147cff1a9cd6e5357a6f
Serhii Kalinets <kalinets@gmail.com>
2023-06-19 19:02:22 +0300

    chore: Trying to make it working

--- cdk_poetry/cdk_poetry_stack.py
+++ cdk_poetry/cdk_poetry_stack.py
@@ -2,7 +2,8 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
-    aws_lambda as _lambda
+    aws_lambda as _lambda,
+    aws_ecr_assets as ecr_assets
 )
 from constructs import Construct
 
@@ -12,9 +13,21 @@ class CdkPoetryStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         func = _lambda.Function(self, "test-function", 
                                 runtime=_lambda.Runtime.PYTHON_3_10,
+                                function_name="test-function",
                                 code=_lambda.Code.from_asset('cdk_poetry/lambdas/hello_world/hello_world'),
                                 handler="index.handler"
                        )
+        func2 =  _lambda.DockerImageFunction(
+            self,
+            "test-docker-function",
+            function_name="test-docker-function",
+            code=_lambda.DockerImageCode.from_image_asset(
+                directory="./cdk_poetry/lambdas/",
+                file=f"hello_world/Dockerfile",
+                platform=ecr_assets.Platform.LINUX_AMD64,
+            ),
+        )
+
 
 
         # The code that defines your stack goes here
